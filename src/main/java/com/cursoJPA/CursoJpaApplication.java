8a8661a5cd2de78e6fd605dffef1f939f90f3490
Nazio Silva de Souza <nazio.silva@gmail.com>
2019-07-24 17:18:25 -0300

    Meu primeiro comite projeto Spring.

---
+++ src/main/java/com/cursoJPA/CursoJpaApplication.java
@@ -0,0 +1,13 @@
+package com.cursoJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoJpaApplication.class, args);
+	}
+
+}
