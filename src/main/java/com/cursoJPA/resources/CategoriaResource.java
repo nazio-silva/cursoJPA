20a4e70e49b2db79ef7083b8127120b4b18193b2
Nazio Silva de Souza <nazio.silva@gmail.com>
2019-07-24 17:59:12 -0300

    Criando classe de dominio Categoria e adicionando itens de categorias.

--- src/main/java/com/cursoJPA/resources/CategoriaResource.java
+++ src/main/java/com/cursoJPA/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.cursoJPA.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cursoJPA.dominio.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "API rest funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria c1 = new Categoria(1, "Informática");
+		Categoria c2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<Categoria>();
+		lista.add(c1);
+		lista.add(c2);
+		
+		return lista;
 	}
 }
